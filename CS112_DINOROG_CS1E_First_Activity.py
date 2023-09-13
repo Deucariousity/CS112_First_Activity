37990e08d600250a52a6f492b13a8bf74f097017
Deucariousity <144906804+Deucariousity@users.noreply.github.com>
2023-09-13 04:48:09 -0700

    Add files via upload

---
+++ CS112_DINOROG_CS1E_First_Activity.py
@@ -0,0 +1,7 @@
+print("Complete Name: Artjohn Clark E. Dinorog")
+print("Course: Bachelor of Science in Computer Science")
+print("Section: BSCS - CS1E")
+print("Age: 21 years old")
+print("Birthday: July 23, 2002")
+print("Address: 224 Blk. 15 Sto. Niño Lapasan Cagayan de Oro City")
+
